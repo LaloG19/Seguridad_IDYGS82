849814423c817a2ac7bb2a9dfedf21152901eee0
LaloG <140776623+LaloG19@users.noreply.github.com>
2024-03-03 16:06:10 -0600

    correo
    asd

---
+++ src/Domain/Entities/Logs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Logs
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public string NombreFuncion { get; set; }
+        public string Ip { get; set; }
+        public string Datos { get; set; }
+        public string Response { get; set; }
+    }
+}
---
+++ src/Domain/Entities/comercial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+
+    [Table("comercial")]
+
+    public class comercial
+    {
+        [Key]
+
+        public int pkcomercial { get; set; }
+
+        public string nombre { get; set; }
+        public string apellido1 { get; set; }
+        public string apellido2 { get; set; }
+        public double comisión { get; set; }
+
+
+
+    }
+}
